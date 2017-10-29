84e0f7d45be85e3ad2066437ce181f679944b5d2
Jessie Qiu <qqqbbbjjj@gmail.com>
2017-10-29 00:10:50 -0700

    updated for shooting to death

--- Assets/Scripts/AnimController2.cs
+++ Assets/Scripts/AnimController2.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class AnimController2 : MonoBehaviour {
 
-    Animator anim;
+    public Animator anim;
     public float speed = 0.8f;
     public float rotationSpeed = 75.0f;
     bool keepwalking = true;
@@ -19,15 +19,16 @@ public class AnimController2 : MonoBehaviour {
 		CapsuleCollider zombieCollider = gameObject.AddComponent<CapsuleCollider> ();
 
 		// // If Unity properly determines the height and radius, then remove this section
-		// zombieCollider.height = 10f;
-		// zombieCollider.radius = 5f;
+		zombieCollider.height = 2f;
+		zombieCollider.radius = 0.5f;
+        zombieCollider.center = new Vector3(0f, 0.75f, 0f);
     }
 
     // Update is called once per frame
     void Update()
     {
-
-        if (keepwalking)
+        if(anim.GetInteger("life")>0)
+    //    if (keepwalking)
         {
             transform.Translate(0, 0, speed * Time.deltaTime);
             //   transform.Translate(Vector3.back * Time.deltaTime * 10);
@@ -37,8 +38,17 @@ public class AnimController2 : MonoBehaviour {
         }
         if (transform.position.z <= -245)
         {
-            anim.SetInteger("life", 0);
+            shot0();
             keepwalking = false;
         }
     }
+
+    public void shot1() {
+        anim.SetInteger("life", 1);
+    }
+    public void shot0()
+    {
+        anim.SetInteger("life", 0);
+    }
+
 }
--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class Gun : MonoBehaviour {
 
     public float damage = 10f;
@@ -9,21 +10,32 @@ public class Gun : MonoBehaviour {
     public Camera fpsCam;
     public ParticleSystem muzzleflash;
     Animator anim;
+    string GunName;
+    GameObject zombie1;
+    AnimController2 otherAnimator;
+    void Awake()
+    {
+        zombie1 = GameObject.Find("Zombie_02_black_Tshirt").GetComponent<GameObject>();
+        if (zombie1 != null)
+        {
+           // otherAnimator = zombie1.GetComponent<Animator>();
+        }
+    }
 
     // Use this for initialization
     void Start () {
         anim = GetComponent<Animator>();
+        // GunName = this.ToString();      
     }
 	
 	// Update is called once per frame
 	void Update () {
         //   if (Input.GetButtonDown("Fire1"))
-        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        if (Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1) || Input.GetButtonDown("Fire1"))
         {
-
             Shoot();
             Debug.Log("Pressed left click.");
-           
+            System.Console.WriteLine("fired");
         }
 
     }
@@ -33,8 +45,35 @@ public class Gun : MonoBehaviour {
         RaycastHit hit;
         if (Physics.Raycast(fpsCam.transform.position, fpsCam.transform.forward, out hit, range))
         {
-           // anim.SetInteger("life", 0);
+           // 
             Debug.Log(message: hit.transform.name + "Fired");
+            print(hit.collider.name);
+            System.Console.WriteLine(hit.collider.name);
+            System.Console.WriteLine(hit.distance);
+            System.Console.WriteLine(GunName);
+            System.Console.WriteLine(hit.transform.name);
+            otherAnimator = hit.collider.gameObject.GetComponent<AnimController2>();
+            if(otherAnimator !=null)
+         //   if (hit.collider.gameObject.name == "Zombie_01_Tshirt" || hit.collider.name == "Terrain Chunk" || hit.collider.name == "Zombie_01_Tshirt" || hit.collider.gameObject.name == "Zombie_02_black_Tshirt"|| hit.transform.name == "Zombie_01_Tshirt") //name ?
+            {
+                
+                if (this.gameObject.name.Equals("Handgun"))
+                {
+                    
+                        otherAnimator.anim.SetInteger("life", 0);
+                    
+                    // otherAnimator.shot1();                   
+                    System.Console.WriteLine("handgun shot zombie");
+                    
+                }
+                else
+                {                    
+                        otherAnimator.anim.SetInteger("life", 0);                    
+                   
+                    System.Console.WriteLine("weapon shot zombie");
+
+                }
+             }
         }
     }
 }
