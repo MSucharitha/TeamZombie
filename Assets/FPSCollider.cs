73f482a6d4a1735219f5c3ee9560ce439684952e
Susi <suchisirigi@gmail.com>
2017-11-02 17:53:38 -0700

    Add FPSCollider for player

---
+++ Assets/FPSCollider.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSCollider : MonoBehaviour {
+    GameObject player;
+    private void Awake()
+    {
+        player = GameObject.FindGameObjectWithTag("MainCamera");
+    }
+    void OnTriggerEnter(Collider col)
+     {
+
+        //System.Console.WriteLine(value:"Collision detected!");
+        Debug.Log("collision detected");
+        if (col.gameObject.GetComponent<AnimController2>() != null)
+        {
+            // ... the player is in range.
+            Debug.Log("Player dead");
+            //System.Console.WriteLine(value: "Player dead");
+
+        }
+    }
+}
---
+++ Assets/FPSHealth.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPSHealth : MonoBehaviour {
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("collision detected");
+
+        if (col.gameObject.GetComponent<AnimController2>() != null)
+        {
+            Debug.Log("Player-Zombie collision");
+        }
+    }
+}
