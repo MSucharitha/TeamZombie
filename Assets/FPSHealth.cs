91a14638e0c785adbda825ef8a35104493e56963
Jessie Qiu <qqqbbbjjj@gmail.com>
2017-11-09 00:26:49 -0800

    files added

---
+++ Assets/FPSHealth.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class FPSHealth : MonoBehaviour {
+    [SerializeField] public int index;
+
+    void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("collision detected");
+
+        if (col.gameObject.GetComponent<AnimController2>() != null)
+        {
+            Debug.Log("Player-Zombie collision");
+            SceneManager.LoadScene(index);
+        }
+    }
+}
