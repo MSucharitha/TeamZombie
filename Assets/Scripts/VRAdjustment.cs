967267ed6ad126335617030ee34016abd4c04123
William <willhnguyen@gmail.com>
2017-12-02 04:45:10 -0800

    Flat terrain with texture completed. Trees missing

--- Assets/Scripts/VRAdjustment.cs
+++ Assets/Scripts/VRAdjustment.cs
@@ -11,6 +11,7 @@ public class VRAdjustment : MonoBehaviour {
 
 	private Vector3 monitorRotation = new Vector3 (-12f, 0f, 0f);
 	private Vector3 openVRPosition = new Vector3 (0f, -1.75f, 0f);
+//	private Vector3 startingPos;
 
 	void Awake () {
 		// If there is a VR device at the start of the game...
@@ -23,6 +24,9 @@ public class VRAdjustment : MonoBehaviour {
 		}
 	}
 
+	void Start() {
+//		startingPos = gameObject.transform.position;
+	}
 
 	void Update () {
 		if (Input.GetKeyUp (KeyCode.R)) {
@@ -33,9 +37,10 @@ public class VRAdjustment : MonoBehaviour {
 		}
 
 		// Reset Position if the Player is falling
-		if (gameObject.transform.position.y < -0.1) {
-			gameObject.transform.Translate (0f, 0.2f, 0f);
-		}
+//		if (gameObject.transform.position.y < -0.1) {
+//			Vector3 currPos = gameObject.transform.position;
+//			gameObject.transform.position = new Vector3 (currPos.x, startingPos.y, currPos.z);
+//		}
 	}
 
 	void OnApplicationQuit() {
