21e3a91e6ce0056edf0d4ba320e1c5cb4696e2de
William <willhnguyen@gmail.com>
2017-12-06 15:58:15 -0800

    Main Menu for HTC Vive Scripts

---
+++ Assets/Scripts/VRUIInput.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(SteamVR_LaserPointer))]
+public class VRUIInput : MonoBehaviour
+{
+	private SteamVR_LaserPointer laserPointer;
+	private SteamVR_TrackedController trackedController;
+
+	private Button button_hover;
+
+	private void OnEnable()
+	{
+		laserPointer = GetComponent<SteamVR_LaserPointer>();
+		laserPointer.PointerIn -= HandlePointerIn;
+		laserPointer.PointerIn += HandlePointerIn;
+		laserPointer.PointerOut -= HandlePointerOut;
+		laserPointer.PointerOut += HandlePointerOut;
+
+		trackedController = GetComponent<SteamVR_TrackedController>();
+		if (trackedController == null)
+		{
+			trackedController = GetComponentInParent<SteamVR_TrackedController>();
+		}
+		trackedController.TriggerClicked -= HandleTriggerClicked;
+		trackedController.TriggerClicked += HandleTriggerClicked;
+	}
+
+	private void HandleTriggerClicked(object sender, ClickedEventArgs e)
+	{
+		if (button_hover != null) {
+			button_hover.onClick.Invoke ();
+		}
+
+		if (EventSystem.current != null && EventSystem.current.currentSelectedGameObject != null)
+		{
+			ExecuteEvents.Execute(EventSystem.current.currentSelectedGameObject, new PointerEventData(EventSystem.current), ExecuteEvents.submitHandler);
+		}
+	}
+
+	private void HandlePointerIn(object sender, PointerEventArgs e)
+	{
+		var button = e.target.GetComponent<Button>();
+		button_hover = button;
+		Debug.Log ("Button in");
+		if (button != null)
+		{
+			button.Select();
+			Debug.Log("HandlePointerIn", e.target.gameObject);
+		}
+	}
+
+	private void HandlePointerOut(object sender, PointerEventArgs e)
+	{
+
+		var button = e.target.GetComponent<Button>();
+		button_hover = null;
+		Debug.Log ("Button out");
+		if (button != null && EventSystem.current != null)
+		{
+			EventSystem.current.SetSelectedGameObject(null);
+			Debug.Log("HandlePointerOut", e.target.gameObject);
+		}
+	}
+}
