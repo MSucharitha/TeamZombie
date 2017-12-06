9f58cf7be7d2567ac3ed3941fbe8cf69156e336c
virajnilakh <virajnilakh220@gmail.com>
2017-12-06 00:47:28 -0800

    added code to modify health UI element

--- Assets/Scripts/FPSHealth.cs
+++ Assets/Scripts/FPSHealth.cs
@@ -7,21 +7,47 @@ using UnityEngine.UI;
 public class FPSHealth : MonoBehaviour {
     [SerializeField] public int index;
     public ApplicationModel app;
+    private Text healthText;
 
-	public int gameOverSceneIndex = 2;
+    public int gameOverSceneIndex = 2;
+    public void healthDamaged()
+    {
+        int health = 0;
+        System.Int32.TryParse(healthText.text.Split(':')[1].TrimEnd('%'), out health);
+        if (health - 25 <= 0)
+        {
+            die();
+        }
+        else
+        {
+            health -= 25;
+            healthText.text = "Health : " + health + "%";
+        }
 
+
+    }
+    public void die()
+    {
+        Text scoreText = GameObject.FindGameObjectWithTag("score").GetComponent<Text>();
+        int score = 0;
+        System.Int32.TryParse(scoreText.text.Split(':')[1], out score);
+        ApplicationModel.score = score;
+        SceneManager.LoadScene(gameOverSceneIndex);
+    }
     void OnTriggerEnter(Collider col)
     {
 //        Debug.Log("collision detected");
 
         if (col.gameObject.GetComponent<AnimController2>() != null)
         {
+            if (healthText == null)
+            {
+                healthText=GameObject.FindGameObjectWithTag("health").GetComponent<Text>();
+            }
+            healthDamaged();
             Debug.Log("Player-Zombie collision");
-            Text scoreText = GameObject.FindGameObjectWithTag("score").GetComponent<Text>();
-            int score = 0;
-            System.Int32.TryParse(scoreText.text.Split(':')[1], out score);
-            ApplicationModel.score = score;
-			SceneManager.LoadScene(gameOverSceneIndex);
+            
         }
     }
+
 }
