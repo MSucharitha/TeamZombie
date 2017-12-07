04a7e01d8940422d7aa65f04bc77c770f8d575cc
William <willhnguyen@gmail.com>
2017-12-07 10:56:42 -0800

    Completed HTC GamePlay

---
+++ Assets/Minimap_VR.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap_VR : MonoBehaviour {
+
+	public GameObject player;
+	public float height;
+	public Vector3 rotate;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = new Vector3 (player.transform.position.x, height, player.transform.position.z);
+		Vector3 playerRotation = player.transform.rotation.eulerAngles;
+		playerRotation = new Vector3 (0f, playerRotation.y, 0f);
+		transform.rotation = Quaternion.Euler (playerRotation);
+		transform.Rotate (rotate);
+
+	}
+}
